1da0b305954c9823ac9fb5c2bc54ac3ec40c0b3e
unknown <konrad.sledziewski17@gmail.com>
2019-03-24 10:28:42 +0100

    Add Azure FaceRecognition library.

--- FaceRecognitionSGHSample/MainPage.xaml.cs
+++ FaceRecognitionSGHSample/MainPage.xaml.cs
@@ -13,6 +13,10 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+
+
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace FaceRecognitionSGHSample
